3d4c1714e8eeec8c9cd02d892c3f7facba0dd432
spacechase-ai <nicholaspfisher21@gmail.com>
2022-01-28 22:48:58 -0500

    Flesh out UI for app, incl. buttons, size slider, color picker, layout, etc.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const grid = document.querySelector(".gridContainer");
+// const colorPicker = document.getElementById("colorPicker");
+// const selectAColor = document.getElementById("selectAColor");
+// const crazyMode = document.getElementById("crazyMode");
+// const eraser = document.getElementById("eraser");
+// const clear = document.getElementById("clear");
+// const range = document.getElementById("range");
+// const sizeSlider = document.getElementById("sizeSlider");
+// colorPicker.onchange = (e) => setCurrentColor(e.color);
+// selectAColor.onclick = () => setCurrentMode("color");
+// crazyMode.onclick = () => setCurrentMode("crazy");
+// eraser.onclick = () => setCurrentMode("eraser");
+// clear.onclick = () => clearGrid();
 let size = 16;
 size = size / 2;
 
